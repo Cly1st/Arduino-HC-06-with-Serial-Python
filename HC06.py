9bbb754420f3e207167f0cd17b7ed7155fb10346
Cly1st <chaylylim@gmail.com>
2020-02-02 19:52:37 +0900

    Add files via upload

---
+++ HC06.py
@@ -0,0 +1,15 @@
+import serial
+
+ser = serial.Serial("COM27", 9600, timeout = 1) #Change your port name COM... and your baudrate
+
+def retrieveData():
+    ser.write(b'1')
+    data = ser.readline().decode('ascii')
+    return data
+
+while(True):
+    uInput = input("Retrieve data? ")
+    if uInput == '1':
+        print(retrieveData())
+    else:
+        ser.write(b'0')
---
+++ Readme.txt
@@ -0,0 +1,2 @@
+Don't forget to make a voltage divider for you HC06 RX pin
+You can check on the internet for that.
